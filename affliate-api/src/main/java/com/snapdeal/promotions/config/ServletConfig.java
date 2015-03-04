d9e105630bc8399c322c09951d88336ee1e06f18
vishal <vishal.vaibhav@snapdeal.com>
2015-03-04 12:42:23 +0530

    Adding provided scope to embedded scope

--- affliate-api/src/main/java/com/snapdeal/promotions/config/ServletConfig.java
+++ affliate-api/src/main/java/com/snapdeal/promotions/config/ServletConfig.java
@@ -2,10 +2,11 @@ package com.snapdeal.promotions.config;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
 import com.snapdeal.promotions.core.Application;
 
-
+@Configuration
 public class ServletConfig extends SpringBootServletInitializer {
 	
 	@Override
--- affliate-api/src/main/java/com/snapdeal/promotions/core/Application.java
+++ affliate-api/src/main/java/com/snapdeal/promotions/core/Application.java
@@ -3,8 +3,9 @@ package com.snapdeal.promotions.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
 @ComponentScan({"com.snapdeal"})
 @EnableAutoConfiguration
 public class Application {
