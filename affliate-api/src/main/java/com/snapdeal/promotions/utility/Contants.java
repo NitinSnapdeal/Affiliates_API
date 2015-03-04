61661f619ac26e50acddd756c5d55b5e6e640949
vishal <vishal.vaibhav@snapdeal.com>
2015-03-04 10:56:53 +0530

    Refactored

--- affliate-api/src/main/java/com/snapdeal/promotions/utility/Contants.java
+++
@@ -1,9 +0,0 @@
-package com.snapdeal.promotions.utility;
-
-public class Contants {
-	
-	public final static String IN_STOCK="In_Stock";
-	
-	public final static String OUT_OF_STOCK="Out_Stock";
-
-}
